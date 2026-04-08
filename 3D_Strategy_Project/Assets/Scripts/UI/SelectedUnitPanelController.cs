39622c755ed966e22be80e9a1c5cd0f68970297e
aNDiQ-cs <nikitin.david2013@yandex.ru>
2026-04-08 16:28:10 +0300

    Start UI rework
    Need to add panel show on hover

---
+++ 3D_Strategy_Project/Assets/Scripts/UI/SelectedUnitPanelController.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+
+public class SelectedUnitPanelController : MonoBehaviour
+{
+    [SerializeField] private MouseResolver m_mouseResolver;
+    [SerializeField] private GameObject m_selectedUnitPanel;
+    [SerializeField] private TMP_Text m_unitNameText;
+
+    private void OnEnable()
+    {
+        m_mouseResolver.OnSelect += ShowPanel;
+        m_mouseResolver.OnDeselect += HidePanel;
+    }
+
+    private void ShowPanel(ISelectable selectable)
+    {
+        MonoBehaviour selectableMono = selectable as MonoBehaviour;
+        m_unitNameText.text = selectableMono.gameObject.name;
+        m_selectedUnitPanel.SetActive(true);
+    }
+
+    private void HidePanel(ISelectable selectable)
+    {
+        m_unitNameText.text = "";
+        m_selectedUnitPanel.SetActive(false);
+    }
+}
